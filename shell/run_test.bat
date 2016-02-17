856670da4b3320814d9b7ce52888610711fe905e
JeffYu <jeff.yu@ndpmedia.com>
2016-02-17 16:22:17 +0800

    add shell

---
+++ shell/run_test.bat
@@ -0,0 +1,5 @@
+python ../script/runner.py 5555 WINDOWS firefox 43.0
+python ../script/runner.py 5555 WINDOWS chrome 48.0
+python ../script/runner.py 5555 WINDOWS MicrosoftEdge 11
+python ../script/runner.py 5555 LINUX chrome 47
+python ../script/runner.py 5555 LINUX firefox 42
