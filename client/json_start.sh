39749f89c01cece90f61b3b8aabb5b608c810fda
JeffYu <jeff.yu@ndpmedia.com>
2016-02-17 17:12:03 +0800

    change chromedriver permission change json_start.sh permission

--- client/json_start.sh
+++ client/json_start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-java -Dwebdriver.chrome.driver="..\driver\chromedriver" -jar selenium-server-standalone.jar -role node -nodeConfig webdriver.json
+java -Dwebdriver.chrome.driver="../driver/chromedriver" -jar ../lib/selenium-server-standalone.jar -role node -nodeConfig webdriver.json
