cfab30c832573ba4a3bcec65d637aa3feba691d0
JeffYu <jeff.yu@ndpmedia.com>
2016-03-01 18:13:45 +0800

    update run_test bat

--- script/run_test.bat
+++ script/run_test.bat
@@ -1,5 +1,3 @@
-python ../src/python/runner.py 5555 WINDOWS firefox 43.0
-python ../src/python/runner.py 5555 WINDOWS chrome 48.0
-python ../src/python/runner.py 5555 WINDOWS MicrosoftEdge 11
-python ../src/python/runner.py 5555 LINUX chrome 47
-python ../src/python/runner.py 5555 LINUX firefox 42
+python ../src/python/grid_test.py 5555 WINDOWS firefox 43.0
+python ../src/python/grid_test.py 5555 WINDOWS chrome 48.0
+python ../src/python/grid_test.py 5555 WINDOWS MicrosoftEdge 11
